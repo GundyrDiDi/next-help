307ff60f9f9d2202c4df7273e386bc0ef3e6e165
shiguang <fenggang@sniffgroup.com>
2024-04-11 15:36:22 +0800

    feat: npm run build:test bug fix

--- src/app/[lang]/help/[cate]/page.tsx
+++ src/app/[lang]/help/[cate]/page.tsx
@@ -2,22 +2,17 @@
  * @Author: shiguang
  * @Date: 2024-04-08 17:04:47
  * @LastEditors: shiguang
- * @LastEditTime: 2024-04-11 15:10:01
+ * @LastEditTime: 2024-04-11 15:35:12
  * @Description: 
  */
 import { request } from "@/config/request"
 import Container from "./Index"
 import axios from "axios";
-import { SupportCenterContentDTO } from "@/service/customer";
 
-
-
-interface PageProps{
+interface HelpCatePageProps{
     params?: {
         cate: string
     }    
-    /** 查询页面的返回值 */
-    serchResultList?: SupportCenterContentDTO[]
 }
 
 /**
@@ -25,32 +20,27 @@ interface PageProps{
  * @param props 
  * @returns 
  */
-export default async function Page(props: PageProps) {
-    const { params, serchResultList } = props;
-    /** 查询页面 */
-    const isSearchPage = Boolean(serchResultList);
-    // https://master-gateway.theckb.com/customer/base/supportCenter/subject/contentList?subjectId=108
-    const resList = await (async () => {
-        if(isSearchPage){
-            return serchResultList!;
-        }
-        const _ = await request.customer.base.supportCenterSubjectContentList({
-            subjectId: params?.cate as unknown as number
-        })
-        return _?.data ?? [];
-    })();
+async function Page(props: HelpCatePageProps) {
+    const { params } = props;
+    const _ = await request.customer.base.supportCenterSubjectContentList({
+        subjectId: params?.cate as unknown as number
+    })
+    const resList =  _?.data ?? [];
+
     let contentList = resList.map((item) => {
         return {
             label: item.title!,
             value: '',
         }
     });
+
     const data = await Promise.all(
         resList.map(item => axios({
             url: item.contentUrl,
             method: 'get'
         }))
     )
+
     contentList = contentList.map((item, index) => {
         const textRes = data[index];
         return {
@@ -58,5 +48,8 @@ export default async function Page(props: PageProps) {
             value: textRes.data.replaceAll('&nbsp;', ' '),
         }
     })
-    return contentList.length ? <Container contentList={contentList} isSearchPage={isSearchPage} /> : null;
+
+    return contentList.length ? <Container contentList={contentList} isSearchPage={false} /> : null;
 }
+
+export default Page;
--- src/app/[lang]/help/search/page.tsx
+++ src/app/[lang]/help/search/page.tsx
@@ -2,11 +2,12 @@
  * @Author: shiguang
  * @Date: 2024-04-08 17:04:47
  * @LastEditors: shiguang
- * @LastEditTime: 2024-04-11 13:41:41
+ * @LastEditTime: 2024-04-11 15:35:50
  * @Description:
  */
+import axios from 'axios';
 import { request } from '@/config/request';
-import CatePage from '../[cate]/page'
+import CatePage from '../[cate]/Index'
 import { getCommonDataByCookie } from '@/utils/server';
 import { getSiteStation } from '@/utils/language';
 import { SupportCenterbizType } from '../utils';
@@ -25,8 +26,28 @@ const Page = async (props: SearchProps) => {
         keyword: props.searchParams.keywords,
         stationCode: getSiteStation()
     })
+    const resList = res.data ?? [];
+    let contentList = resList.map((item) => {
+        return {
+            label: item.title!,
+            value: '',
+        }
+    });
+    const data = await Promise.all(
+        resList.map(item => axios({
+            url: item.contentUrl,
+            method: 'get'
+        }))
+    )
+    contentList = contentList.map((item, index) => {
+        const textRes = data[index];
+        return {
+            label: item.label!,
+            value: textRes.data.replaceAll('&nbsp;', ' '),
+        }
+    })
     return <div>
-        <CatePage serchResultList={ (res.data ?? []) } />
+        <CatePage contentList={contentList} isSearchPage={true} />
     </div>
 }
 export default Page
