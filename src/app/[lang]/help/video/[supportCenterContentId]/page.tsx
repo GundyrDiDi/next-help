81a39490ef2d694ddcbbe6b8d677a115c09cf0c0
shiguang <fenggang@sniffgroup.com>
2024-04-12 16:07:33 +0800

    feat: add video play page seo

--- src/app/[lang]/help/video/[supportCenterContentId]/page.tsx
+++ src/app/[lang]/help/video/[supportCenterContentId]/page.tsx
@@ -2,15 +2,69 @@
  * @Author: shiguang
  * @Date: 2024-04-09 16:28:24
  * @LastEditors: shiguang
- * @LastEditTime: 2024-04-10 16:20:50
+ * @LastEditTime: 2024-04-12 16:06:12
  * @Description: 
  */
 import { request } from "@/config/request";
+import { Local } from "@/i18n/settings";
+import { Metadata } from "next";
 interface PageProps {
     params: {
-        supportCenterContentId: string
+        supportCenterContentId: string;
+        lang: Local;
     }
 }
+
+const getMetaConf = async ({supportCenterContentId, lang}: PageProps['params']) => {
+    const res = await request.customer.base.supportCenterQueryGetSupportCenterContentById({
+        contentId: supportCenterContentId as unknown as number
+    })
+    const description = res.data?.description;
+    
+    const conf: Record<Local, Metadata> = {
+        [Local.JA]: {
+            title: `THE CKBヘルプガイド |${res.data?.title}`,
+            description,
+        },
+        [Local.KO]: {
+            title: undefined,
+            description,
+        },
+        [Local.EN]: {
+            title: undefined,
+            description,
+        }
+    };
+    return conf[lang]
+}
+
+export async function generateMetadata(
+    { params }: { params: PageProps['params'] },
+): Promise<Metadata> {
+    const metaConf = await getMetaConf(params)
+    const title =  metaConf?.title ?? 'THE CKB';
+    const description =  metaConf?.description ?? 'THE CKB';
+    return {
+        metadataBase: new URL("https://s.theckb.com/"),
+        alternates: {
+            canonical: "/",
+            languages: {
+                en: "/en",
+                ko: "/ko",
+                ja: "/ja",
+            },
+        },
+        title: title,
+        description,
+        openGraph: {
+            siteName: "THE CKB",
+            images: "https://static-s.theckb.com/BusinessMarket/Client/favicon.ico",
+            title,
+            description,
+        },
+    };
+}
+
 
 // 通过 supportCenterContentId 查询内容
 
@@ -23,6 +77,7 @@ const Page = async (props: PageProps) => {
 
     const videoUrl = res.data?.contentUrl
     return <div>
+        <h1 className="hidden" >{res.data?.title}</h1>
         <video controls className="h-[calc(100vh-32px)] w-[100%] bg-[#262626]" >
             {!!videoUrl && <source src={videoUrl} />}
         </video>
