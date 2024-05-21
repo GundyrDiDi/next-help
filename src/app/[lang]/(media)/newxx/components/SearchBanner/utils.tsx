31f94f3ac3addf093bad06c16c91d8320806e815
shiguang <fenggang@sniffgroup.com>
2024-05-21 14:48:52 +0800

    feat: frogArticleTypeId

--- src/app/[lang]/(media)/newxx/components/SearchBanner/utils.tsx
+++ src/app/[lang]/(media)/newxx/components/SearchBanner/utils.tsx
@@ -2,11 +2,12 @@
  * @Author: shiguang
  * @Date: 2024-05-17 16:28:04
  * @LastEditors: shiguang
- * @LastEditTime: 2024-05-17 17:02:32
+ * @LastEditTime: 2024-05-21 14:48:23
  * @Description: 
  */
 import { atom } from "jotai";
 import { useParams, usePathname } from "next/navigation";
+import { useTranslation } from "react-i18next";
 
 /** 青蛙文章 当前状态是否是阅读记录 */
 export const atomIsArticalReadRecord = atom(false);
@@ -32,6 +33,7 @@ export const ReadRecordMenuItem = (props: ReadRecordMenuItem) => {
     const { closeMenu } = props;
     const pathName = usePathname()
     const { lang: _paramsLang } = useParams()
+    const { t } = useTranslation();
     const isHomePage = `/${_paramsLang}` === pathName;
     if (!isHomePage) return null;
     return <div
@@ -42,6 +44,6 @@ export const ReadRecordMenuItem = (props: ReadRecordMenuItem) => {
         className="bg-[#fff] mo:text-center mo:h-[50px] flex items-center justify-center text-[--color-primary] text-[16px] "
         style={{ borderBottom: '1px solid #E6EAF0' }}
     >
-        浏览记录
+        {t('浏览记录')}
     </div>
 }
