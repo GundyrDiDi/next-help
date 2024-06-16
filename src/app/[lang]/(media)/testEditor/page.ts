61c9b5b287e129d785cbb7009a35c49344efbdef
shiguang <fenggang@sniffgroup.com>
2024-06-16 22:26:33 +0800

    feat: add lib

---
+++ src/app/[lang]/(media)/testEditor/page.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: shiguang
+ * @Date: 2024-06-16 22:24:42
+ * @LastEditors: shiguang
+ * @LastEditTime: 2024-06-16 22:24:58
+ * @Description: 
+ */
+const testEditor = () => {
+
+}
+export default testEditor;
